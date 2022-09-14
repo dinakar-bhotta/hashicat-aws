5d75c36a2f47c24823ef0cda8c0a93f81988ae8e
dinakar-bhotta <dinakar.bhotta@yahoo.com>
2022-09-14 05:43:10 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/dinakar-bhotta/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "${var.bucket_prefix}-s3-bucket-140922"
   acl    = "private"
 
   versioning = {
